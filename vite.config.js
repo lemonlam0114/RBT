b6cb883935a67d1ad2b5fce06ee09880c585c4dd
lemonlam0114 <lemonlam0114@gmail.com>
2025-03-28 14:41:40 +0800

    描述你此次更新的内容

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  server: {
-    port: 3000
-  }
+  plugins: [react()]
 })
